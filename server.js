d311220972658a46d56fa47af64fc5dcbd49166c
ankurInfinity27 <ankur.rathi.infinitylabs.in>
2018-07-11 15:35:13 +0530

    Add post route

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
-
+const _= require('lodash');
 const express=require('express');
 const bodyParser=require('body-parser');
 
 const {mongoose}=require('./server/db/mongoose');
 var {Todo}=require('./server/models/todo');
-var {Users}=require('./server/models/user');
+var {User}=require('./server/models/user');
 
 
 // const hbs=require('hbs');
@@ -65,6 +65,72 @@ app.get('/todos',(req,res)=>{
          console.log("Error",err);
          res.status(400).send(err);
      }
+)
+});
+
+app.post('/users',(req,res)=>{
+   console.log("Req",req.body);
+   var body=_.pick(req.body,['email','password']);
+   console.log("boyd",body);
+   var user=new User(body);
+      
+   user.save().then((data)=>{
+       console.log("dsad",data);
+       return user.generateAuthToken();
+
+   }).then((token)=>{
+    res.header('x-auth',token).status(200).send(user);
+   }).catch((err)=>{
+       console.log("Error",err.code);
+       const errMsg=`${req.body.email} already exists!!`;
+       if(err.code===11000){
+        return res.status(400).send(errMsg);
+       }
+    res.status(400).send(err);
+   });
+
+});
+
+
+app.delete('/todos/:id',(req,res)=>{
+  var todo=new Todo();
+
+  console.log("ID",req.params.id);
+  Todo.findOneAndRemove({_id:req.params.id}).then((result)=>{
+       res.status(201).send(result);
+  },
+ (err)=>{
+     console.log("Error",err);
+     res.status(400).send(err);
+ }
+)
+});
+
+
+app.patch('/todos/:id',(req,res)=>{
+    var id=req.params.id;
+    var todo=new Todo();
+    var body=_.pick(req.body,['text','completed']);
+
+    if(_.isBoolean(body.completed) && body.completed){
+          body.completedAt=new Date().getTime();
+    }
+    else{
+        body.completed=false;
+        body.completedAt=null;
+    }
+
+    Todo.findByIdAndUpdate(id,{$set:body},{new:true}).then((todo)=>{
+              
+        if(!todo){
+            res.status(404).send();
+        }
+        res.send(todo);
+    },
+    (err)=>{
+        console.log("Error",err);
+        res.status(400).send();
+    }
 )
 });
 
